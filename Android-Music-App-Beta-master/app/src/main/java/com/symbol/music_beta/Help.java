db02e1cec6370c786f0d68295d5f68923b0dbad2
David DeFazio <davedefazio1@gmail.com>
2015-07-29 16:20:10 -0400

    Create Help.java

---
+++ Android-Music-App-Beta-master/app/src/main/java/com/symbol/music_beta/Help.java
@@ -0,0 +1,12 @@
+package com.symbol.music_beta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Help extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
